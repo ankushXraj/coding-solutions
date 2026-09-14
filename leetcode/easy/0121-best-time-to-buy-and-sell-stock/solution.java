920cd7d57786f74fceb4cbeee9b478f6152b4502
Ankush Raj <rajankush0480@gmail.com>
2026-09-14 19:04:39 +0530

    ✅ Best Time to Buy and Sell Stock | Easy | Java | Runtime: 2 ms (45.16%) | Memory: 94.4 MB | 2026-09-14

--- leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
+++ leetcode/easy/0121-best-time-to-buy-and-sell-stock/solution.java
@@ -1,25 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) {
-            return 0;
-        }
-        
         int minPrice = prices[0];
         int maxProfit = 0;
-        
-        for (int i = 1; i < prices.length; i++) {
-            // Update minPrice if we find a lower buying price
-            if (prices[i] < minPrice) {
+        for (int i = 1 ; i < prices.length ; i++){
+            if (prices[i] <minPrice )
+            {
                 minPrice = prices[i];
+
             } else {
-                // Calculate profit and update maxProfit if it's higher
-                int currentProfit = prices[i] - minPrice;
-                if (currentProfit > maxProfit) {
-                    maxProfit = currentProfit;
-                }
+                maxProfit = Math.max(maxProfit , prices[i]- minPrice );
+
             }
-        }
-        
+        } 
         return maxProfit;
     }
 }
